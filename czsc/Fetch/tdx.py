918876abf3f237e8f400ea6a8e7018fab4c6f3b3
newlyedward <newlyedward>
2021-01-10 00:56:20 +0800

    统一从tdx本地硬盘取行情数据的接口

--- czsc/Fetch/tdx.py
+++ czsc/Fetch/tdx.py
@@ -23,106 +23,257 @@
 # SOFTWARE.
 
 # 从TDX磁盘空间读取数据
-import datetime
 import os
 import re
+
 import pandas as pd
-import numpy as np
+from pandas import DataFrame
+
+from pytdx.reader import TdxDailyBarReader, TdxExHqDailyBarReader, TdxMinBarReader, TdxLCMinBarReader
 
 from czsc.Setting import TDX_DIR
 from czsc.Utils import util_log_info
 
-DS_DIR = '{}{}{}'.format(TDX_DIR, os.sep, 'vipdoc\\ds')
+_SH_DIR = '{}{}{}'.format(TDX_DIR, os.sep, 'vipdoc\\sh')
+_SZ_DIR = '{}{}{}'.format(TDX_DIR, os.sep, 'vipdoc\\sz')
+_DS_DIR = '{}{}{}'.format(TDX_DIR, os.sep, 'vipdoc\\ds')
 
 
-def fetch_future_list(market='all'):
+def _get_sh_sz_list():
     """
+    读取上海深圳交易所行情目录的文件列表，并对市场，品种和代码分类
+    sh000015.day   期货    ('28', 'AP2003')
+    'sse'     # 上海证券交易所       sh  6位数字代码
+               前两位 "60"    A股
+                     "90"    B股
+                     "00", "88", "99" 指数
+                     "50", "51"       基金
+                     "01", "10", "11", "12", "13", "14" 债券，和深圳有重合
+
+    'szse'    # 深圳证券交易所       sz  6位数字代码
+            前两位 "00", "30"  A股
+                  "20"
+                  "39"       指数
+                  "15", "16" 基金
+                  "10", "11", "12", "13", "14" 债券，和深圳有重合
+    pattern = "^(?P<tdx_code>[shz]{2})#(?P<code>\d{6})\.day"
+    """
+    sh_dir = '{}{}{}'.format(_SH_DIR, os.sep, 'lday')
+    sh_list = os.listdir(sh_dir)
+
+    pattern = "^(?P<tdx_code>sh)(?P<code>\d{6})\.day"
+    data = [re.match(pattern, x) for x in sh_list]
+    try:
+        sh_df = pd.DataFrame([x.groupdict() for x in data])
+    except:
+        util_log_info("{} can't be analyzed by pattern ({}) }".format(_SH_DIR, pattern))
+        return None
+
+    sh_df['exchange'] = 'sse'
+
+    SH_CODE_HEAD_TO_TYPE = {
+        '60': 'stock',
+        '68': 'stock',  # 科创板
+        '90': 'stock',  # B股
+        '00': 'index',
+        '88': 'index',
+        '99': 'index',
+        '50': 'fund',
+        '51': 'fund',
+        '58': 'ETF',   # 科创板ETF
+        '20': 'bond',  # 国债逆回购
+        '01': 'bond',
+        '10': 'bond',
+        '11': 'bond',
+        '12': 'bond',
+        '13': 'bond',
+        '14': 'bond',
+    }
+
+    sh_df['instrument'] = sh_df.code.apply(lambda x: SH_CODE_HEAD_TO_TYPE[x[:2]])
+
+    sz_dir = '{}{}{}'.format(_SZ_DIR, os.sep, 'lday')
+    sz_list = os.listdir(sz_dir)
+
+    pattern = "^(?P<tdx_code>sz)(?P<code>\d{6})\.day"
+    data = [re.match(pattern, x) for x in sz_list]
+    try:
+        sz_df = pd.DataFrame([x.groupdict() for x in data])
+    except:
+        util_log_info("{} can't be analyzed by pattern ({}) }".format(_SZ_DIR, pattern))
+        return None
+
+    sz_df['exchange'] = 'szse'
+
+    SZ_CODE_HEAD_TO_TYPE = {
+        '00': 'stock',  # 中小板 主板
+        '30': 'stock',  # 创业板
+        '20': 'stock',  # B股
+        '39': 'index',
+        '15': 'fund',
+        '16': 'fund',
+        '10': 'bond',
+        '11': 'bond',
+        '12': 'bond',
+        '13': 'bond',   # 逆回购
+        '18': 'reits',  # REITS
+    }
+    sz_df['instrument'] = sz_df.code.apply(lambda x: SZ_CODE_HEAD_TO_TYPE[x[:2]])
+
+    return pd.concat([sh_df, sz_df])
+
+
+def _get_ds_list():
+    """
+    读取扩展行情目录的文件列表，并对市场，品种和代码分类
     47#TS2009.day   期货    ('28', 'AP2003')
     7#IO760795.day  期权    ('7', 'IO760795')
     5#V 7C0D49.day  期权 中间有空格，特殊处理
-    pattern = "^(?P<market>\d{1,2})#(?P<code>.+)\.day"
-    market: all 包含所有期货和期权
-            future 只返回期货
-            option 只返回期权
-            'cffex',  # 中金所             47
-            'czce',   # 郑州商品交易所       28
-            'dce',    # 大连商品交易所       29
-            'shfe',   # 上海期货交易所       30
+    pattern = "^(?P<tdx_code>\d{1,2})#(?P<code>.+)\.day"
     """
-    lday_dir = '{}{}{}'.format(DS_DIR, os.sep, 'lday')
-    ds_list = os.listdir(lday_dir)
-    market = market.upper()
+    DS_CODE_TO_TYPE = {
+        '4': {'exchange': 'czce', 'instrument': 'option'},
+        '5': {'exchange': 'dce', 'instrument': 'option'},
+        '6': {'exchange': 'shfe', 'instrument': 'option'},
+        '7': {'exchange': 'cffex', 'instrument': 'option'},
+        '8': {'exchange': 'sse', 'instrument': 'option'},
+        '9': {'exchange': 'szse', 'instrument': 'option'},
+        '28': {'exchange': 'czce', 'instrument': 'future'},
+        '29': {'exchange': 'dce', 'instrument': 'future'},
+        '30': {'exchange': 'shfe', 'instrument': 'future'},
+        '47': {'exchange': 'cffex', 'instrument': 'future'},
+    }
+    ds_dir = '{}{}{}'.format(_DS_DIR, os.sep, 'lday')
+    ds_list = os.listdir(ds_dir)
 
-    pattern = "^(?P<market>\d{1,2})#(?P<code>.+)\.day"
+    pattern = "^(?P<tdx_code>\d{1,2})#(?P<code>.+)\.day"
     data = [re.match(pattern, x) for x in ds_list]
     try:
-        future_list = pd.DataFrame([x.groupdict() for x in data])
+        ds_df = pd.DataFrame([x.groupdict() for x in data])
     except:
-        util_log_info("{} can't be analyzed by pattern ({}) }".format(ds_dir, pattern))
+        util_log_info("{} can't be analyzed by pattern ({}) }".format(_DS_DIR, pattern))
         return None
 
-    # todo 根据 市场和品种 代码返回 list
-    if market == 'ALL':
-        return future_list
+    ds_df['exchange'] = ds_df.tdx_code.apply(lambda x: DS_CODE_TO_TYPE[x]['exchange'] if x in DS_CODE_TO_TYPE else None)
+    ds_df['instrument'] = ds_df.tdx_code.apply(
+        lambda x: DS_CODE_TO_TYPE[x]['instrument'] if x in DS_CODE_TO_TYPE else None)
+
+    return ds_df
 
 
-def get_future_market_from_code(code):
-    """
-    返回 TDX 的市场代码
-    """
+def get_security_list():
+    securities: DataFrame = pd.concat([_get_sh_sz_list(), _get_ds_list()])
+    return securities.set_index('code')
+
+
+SECURITY_DATAFRAME = get_security_list()
+
+
+def _get_tdx_code_from_security_dataframe(code, exchange):
+    try:
+        recorder = SECURITY_DATAFRAME.loc[code]
+    except:
+        util_log_info("Can't get tdx_code from {}".format(code))
+        return
+
+    if len(recorder) < 2:
+        return recorder.loc[:, 'tdx_code']
+
+    try:
+        return recorder.loc[recorder['exchange'] == exchange].loc[code, 'tdx_code']
+    except:
+        util_log_info('Not only one {} in the list , please provide exchange or instrument'.format(code))
+        return recorder.tdx_code[0]
+
+
+def _generate_path(code, freq, tdx_code):
+    # code = code.upper()
+    # freq = freq.lower()
+
+    ext = {
+        'day': '.day',
+        '5min': '.lc5',
+        '1min': '.lc1',
+    }
+
+    dir = {
+        'day': 'lday',
+        '5min': 'fzline',
+        '1min': '.minline',
+    }
+
+    try:
+        if tdx_code == 'sz':
+            dir_name = '{}{}{}'.format(_SZ_DIR, os.sep, dir[freq])
+            filename = tdx_code + code + ext[freq]
+        elif tdx_code == 'sh':
+            dir_name = '{}{}{}'.format(_SH_DIR, os.sep, dir[freq])
+            filename = tdx_code + code + ext[freq]
+        else:
+            dir_name = '{}{}{}'.format(_DS_DIR, os.sep, dir[freq])
+            filename = tdx_code + '#' + code + ext[freq]
+    except KeyError:
+        util_log_info('Not supported Frequency {}!'.format(freq))
+        return
+
+    file_path = os.path.join(dir_name, filename)
+    return file_path
+
+
+def get_bar(code, freq='day', exchange=None):
     code = code.upper()
-    future_list = fetch_future_list()
-    # todo 以后改为返回字符格式或者代码格式两种方式
-    return future_list[future_list['code'] == code].iloc[0, 0]
+    freq = freq.lower()
 
+    try:
+        tdx_code = _get_tdx_code_from_security_dataframe(code, exchange)
+    except:
+        util_log_info("Can't get tdx_code from {}".format(code))
+        return
 
-def fetch_future_day(code, start=None, end=None):
-    """
+    file_path = _generate_path(code, freq, tdx_code)
 
-    """
-    lday_dir = '{}{}{}'.format(DS_DIR, os.sep, 'lday')
-    market = get_future_market_from_code(code)
-    filename = market + '#' + code.upper() + '.day'
-    file_path = os.path.join(lday_dir, filename)
+    if not file_path:
+        return
 
-    if not os.path.exists(file_path):
-        util_log_info('{} hq is not exists!'.format(code))
-        return None
+    if tdx_code in ['sh', 'sz']:
+        if freq in ['day']:
+            reader = TdxDailyBarReader()
+            df = reader.get_df(file_path)
+        elif freq in ['1min', '5min']:
+            reader = TdxLCMinBarReader()
+            df = reader.get_df(file_path)
+        else:
+            util_log_info('Not supported frequency {}'.format(freq))
+            return
+    else:
+        if freq in ['day']:
+            reader = TdxExHqDailyBarReader()
+            df = reader.get_df(file_path)
+        elif freq in ['1min', '5min']:
+            reader = TdxLCMinBarReader()
+            df = reader.get_df(file_path)
+        else:
+            util_log_info('Not supported frequency {}'.format(freq))
+            return
 
-    f = open(file_path, "rb")
+    if len(df) < 1:
+        return
 
-    def _tdx_future_day_hq(file_handler):
-        # 和QA的字段名
-        names = 'date', 'open', 'high', 'low', 'close', 'position', 'trade', 'comment'
-        offsets = tuple(range(0, 31, 4))
-        formats = 'i4', 'f4', 'f4', 'f4', 'f4', 'i4', 'i4', 'i4'
+    recorder = SECURITY_DATAFRAME.loc[code]
 
-        dt_types = np.dtype({'names': names, 'offsets': offsets, 'formats': formats}, align=True)
-        hq = pd.DataFrame(np.fromfile(file_handler, dt_types))
-        hq['date'] = hq['date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
-        hq.set_index('date', inplace=True, drop=False)
-        hq = hq.assign(code=code)
-        return hq
+    if len(recorder) > 1:
+        instrument = recorder.loc[recorder['tdx_code'] == tdx_code].loc[code, 'instrument']
+    else:
+        instrument = recorder['instrument']
 
-    start = pd.to_datetime('1990-01-01', format='%Y-%m-%d') if start is None else pd.to_datetime(start)
-    end = datetime.date.today().strftime('%Y-%m-%d') if end is None else pd.to_datetime(end)
+    if instrument in ['future', 'option']:
+        return df.rename(columns={'amount': "position", "jiesuan": "settle"})
 
-    data = _tdx_future_day_hq(f)
-
-    if start is not None:
-        start = pd.to_datetime(start)
-        data = data[start <= data.index]
-
-    if end is not None:
-        end = pd.to_datetime(end)
-        data = data[data.index <= end]
-
-    return data
-
+    return df
 
 if __name__ == "__main__":
-    df = fetch_future_list()
-    hq = fetch_future_day('rbl8')
-
-
-
+    # ds_df = _get_ds_list()
+    # sz_sh_df = _get_sh_sz_list()
+    # security_df = get_security_list()
+    # hq = fetch_future_day('rbl8')
+    hq = get_bar('000001')
